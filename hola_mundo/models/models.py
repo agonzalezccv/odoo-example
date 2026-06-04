a5aa3b6581b0e9994a72461c8a41eec16e23df56
Anderson Gonzalez <GONZALEZA@venequip.com>
2026-06-04 08:57:41 -0400

    Prueba de Herencia del modulo Contacto

--- hola_mundo/models/models.py
+++ hola_mundo/models/models.py
@@ -8,3 +8,14 @@ class HolaMundoTarea(models.Model):
     name = fields.Char(string='Título de la Tarea', required=True)
     description = fields.Text(string='Descripción')
     is_done = fields.Boolean(string='¿Completada?', default=False)
+
+
+# Prueba para heredar  de Contactos y agregar un nuevo campo Status
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    is_test_partner = fields.Boolean(
+        string='¿Es de prueba?', 
+        default=False,
+        help='Indica si este contacto es usado para pruebas internas.'
+    )
