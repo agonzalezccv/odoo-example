684cac62245b523be959ede70037d4b1c55c3b28
Anderson Gonzalez <GONZALEZA@venequip.com>
2026-06-04 08:48:21 -0400

    feat: arranque limpio de hola mundo en odoo 19 v6

--- hola_mundo/__manifest__.py
+++ hola_mundo/__manifest__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Hola Mundo",
-    'summary': "Mi primer módulo de prueba en el Bootcamp",
+    'summary': "Mi primer módulo de prueba en Odoo .sh",
     'version': '19.0.1.0.0',
-    'author': "Yardi",
-    'category': 'Uncategorized',
+    'author': "Anderson Gonzalez",
+    'category': 'Pruebas',
     'depends': ['base'],
     'data': [
          'views/views.xml',
